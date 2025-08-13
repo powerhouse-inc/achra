302442e74c61be4af0be214ac43a26fee168c416
Yasiel Cabrera <yasiel9506@gmail.com>
2025-08-12 22:30:32 -0400

    feat: add storybook theme switch

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,18 @@
 import type { Preview } from '@storybook/nextjs-vite'
+import { withThemeByClassName } from '@storybook/addon-themes'
 
 import '../app/globals.css'
 
 const preview: Preview = {
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
   parameters: {
     controls: {
       matchers: {
