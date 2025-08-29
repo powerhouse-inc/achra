9427ca5e8aadd4fc59f1eac476cb971c252bb634
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-08-29 19:55:35 -0300

    fix(achra): improve the nextConfig add webpack production

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config
+  },
   turbopack: {
     rules: {
       '*.svg': {
