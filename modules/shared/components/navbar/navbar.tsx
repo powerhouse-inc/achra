2ac05b8d9049a80ccd39614ce94c12369e952480
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-02 14:45:15 -0300

    style(achra): add style for navbar and add correct structure in storybook

--- modules/shared/components/navbar/navbar.tsx
+++ modules/shared/components/navbar/navbar.tsx
@@ -3,8 +3,8 @@ import { ExternalLink } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import AchraLogo from '@/modules/shared/components/svgs/achra-logo.svg'
-import KebabMenu from '@/modules/shared/components/svgs/kebab-menu.svg'
 import { cn } from '../../lib/utils'
+import { KebabMenu } from '../svgs'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import { Button } from '../ui/button'
 import { getNavbarConfig } from './navbar-config'
@@ -16,26 +16,23 @@ function Navbar() {
 
   const config = getNavbarConfig(pathname)
 
-  const { isotype: Isotype, logotype: Logotype, navItems } = config
+  const { isotype: BrandLogo, logotype: BrandLogotype, navItems } = config
 
   return (
-    <header className="bg-background/50 border2 sticky top-0 mx-auto flex items-center rounded-3xl backdrop-blur md:top-1.5 md:p-2.5">
+    <header className="border2 bg-muted/50 sticky top-0 mx-auto flex max-w-[var(--container-width)] items-center justify-center rounded-3xl backdrop-blur-[7.5px] md:top-1.5 md:p-2.5">
       <div className="bg-card flex flex-1 items-center justify-between rounded-2xl pr-4 md:pr-4">
         <div className="flex w-full items-center justify-between">
           <div className="flex items-center gap-4 md:gap-6">
-            <div className="text-border bg-background border-border flex items-center justify-center rounded-l-lg border-r-2 px-4 py-3.5 md:px-6 md:py-4.5">
+            <div className="text-border bg-primary/5 border-border flex items-center justify-center rounded-l-lg border-r-1 px-4 py-3.5 md:px-6 md:py-4.5">
               <AchraLogo className="h-9 w-9" />
             </div>
             <div className="flex items-center gap-2">
-              {Isotype && <Isotype className="h-8 w-8" />}
-              {Logotype && <Logotype className="hidden:md:flex h-8 w-16" />}
+              {BrandLogo && <BrandLogo className="h-8 w-8" />}
+              {BrandLogotype && <BrandLogotype className="hidden:md:flex h-8 w-16" />}
             </div>
             <div className="flex min-w-37.75 items-center gap-8 md:hidden">
               <div>Select Network</div>
             </div>
-          </div>
-          <div className="text-background flex items-center md:hidden">
-            <KebabMenu className="h-9 w-9" />
           </div>
 
           <nav className="hidden items-center gap-2 md:flex md:gap-4 lg:gap-12">
@@ -57,6 +54,9 @@ function Navbar() {
               )
             })}
           </nav>
+          <div className="text-background flex items-center md:hidden">
+            <KebabMenu className="h-9 w-9" />
+          </div>
           {!isLoggedIn ? (
             <div className="hidden items-center md:flex">
               <Button variant="outline">Log in</Button>
@@ -65,7 +65,6 @@ function Navbar() {
             <div className="hidden items-center md:flex">
               <Avatar>
                 <AvatarImage src="https://i.pravatar.cc/100?img=5" alt="avatar" />
-
                 <AvatarFallback>JD</AvatarFallback>
               </Avatar>
             </div>
