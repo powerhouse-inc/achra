3a656a9eacdd9d6e9da80e04ac0b1a0af01b6992
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-03 20:54:35 -0300

    fix(achra): fix the formatting and remove properties from svg

--- modules/shared/components/navbar/components/navbar-brand.tsx
+++ modules/shared/components/navbar/components/navbar-brand.tsx
@@ -1,31 +1,30 @@
-import Link from 'next/link';
-import AchraLogo from '@/modules/shared/components/svgs/achra-logo.svg';
-import AchraIsotype from '@/modules/shared/components/svgs/achra-imagotipo.svg';
-import { cn } from '@/modules/shared/lib/utils';
-import React from 'react';
+import Link from 'next/link'
+import AchraLogo from '@/modules/shared/components/svgs/achra-logo.svg'
+import AchraIsotype from '@/modules/shared/components/svgs/achra-imagotipo.svg'
+import { cn } from '@/modules/shared/lib/utils'
+import React from 'react'
 
 interface NavbarBrandProps {
-  isNetworksPage: boolean;
-  BrandLogo?: React.ElementType;
-  BrandLogotype?: React.ElementType;
+  isNetworksPage: boolean
+  BrandLogo?: React.ElementType
+  BrandLogotype?: React.ElementType
 }
 
 export function NavbarBrand({ isNetworksPage, BrandLogo, BrandLogotype }: NavbarBrandProps) {
-
-  const MainLogoComponent = isNetworksPage ? AchraIsotype : AchraLogo;
+  const MainLogoComponent = isNetworksPage ? AchraIsotype : AchraLogo
   const logoContainerClasses = cn(
     'text-border flex items-center justify-center overflow-hidden py-4.5',
     {
       'rounded-l-2xl border-r bg-primary/5 border-border px-4 md:px-6': !isNetworksPage,
       'pl-6': isNetworksPage,
-    }
-  );
+    },
+  )
 
   return (
     <div className="flex items-center gap-4 md:gap-6">
       <div className={logoContainerClasses}>
         <Link href="/networks">
-          <MainLogoComponent className={cn('h-9', isNetworksPage ? 'w-42 text-primary' : 'w-9')} />
+          <MainLogoComponent className={cn('h-9', isNetworksPage ? 'text-primary w-42' : 'w-9')} />
         </Link>
       </div>
       {!isNetworksPage && BrandLogo && BrandLogotype && (
@@ -35,5 +34,5 @@ export function NavbarBrand({ isNetworksPage, BrandLogo, BrandLogotype }: Navbar
         </div>
       )}
     </div>
-  );
+  )
 }
