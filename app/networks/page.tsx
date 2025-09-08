ebc92e033c92a2a32bbe3a5579ffec7d5516eedf
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-08 10:59:38 -0300

    feat(achra): group image and fix the fontSize

--- app/networks/page.tsx
+++ app/networks/page.tsx
@@ -32,7 +32,7 @@ const networks: Network[] = [
       'Powerhouse is the central network within the Sky ecosystem, dedicated to gathering and simplifying data for all users. It ensures that information is not only accessible but also easy to understand.',
     buttonText: 'Explore Powerhouse ',
     bgGradient: 'from-purple-500/20 to-blue-500/20',
-    backgroundImage: '/powerhouse-card-background.png',
+    backgroundImage: '/network-backgrounds/powerhouse-card-background.png',
   },
   {
     id: 'sky',
@@ -45,7 +45,7 @@ const networks: Network[] = [
       'Sky serves as a pivotal hub within the ecosystem, where users can access a wealth of resources and tools. This platform empowers exploration and innovation, enabling seamless interactions and collaboration among various stakeholders.',
     buttonText: 'Explore Sky ',
     bgGradient: 'from-pink-500/20 to-purple-500/20',
-    backgroundImage: '/sky-card-background.png',
+    backgroundImage: '/network-backgrounds/sky-card-background.png',
   },
   {
     id: 'spark',
@@ -58,7 +58,7 @@ const networks: Network[] = [
       "Spark is a dynamic network within the Powerhouse ecosystem, designed to amplify the platform's capabilities. By facilitating seamless interactions and resource sharing, Spark empowers users to innovate and collaborate effectively.",
     buttonText: 'Explore Spark ',
     bgGradient: 'from-green-400/20 to-white/20',
-    backgroundImage: '/spark-card-background.png',
+    backgroundImage: 'network-backgrounds/spark-card-background.png',
   },
   {
     id: 'grove',
@@ -71,7 +71,7 @@ const networks: Network[] = [
       'Grove is a vital network within the Powerhouse platform, dedicated to fostering growth and nurturing its builders. By providing essential resources and support, Grove cultivates an environment where innovation thrives.',
     buttonText: 'Explore Grove ',
     bgGradient: 'from-green-600/20 to-emerald-500/20',
-    backgroundImage: '/grove-card-background.png',
+    backgroundImage: 'network-backgrounds/grove-card-background.png',
   },
   {
     id: 'launch-agent-2',
@@ -84,7 +84,7 @@ const networks: Network[] = [
       'Launch Agent 2 is a pivotal network within the Powerhouse ecosystem, designed to enhance connectivity and streamline operations. By utilizing cutting-edge technology, it fosters collaboration among various platforms.',
     buttonText: 'Explore Launch Agent 2 ',
     bgGradient: 'from-orange-500/20 to-red-500/20',
-    backgroundImage: '/image.png',
+    backgroundImage: 'network-backgrounds/launch-agent-2-card-background.png',
   },
   {
     id: 'launch-agent-3',
@@ -97,7 +97,7 @@ const networks: Network[] = [
       'Launch Agent 3 is a cutting-edge network within the Powerhouse ecosystem, designed to optimize connectivity and streamline operations.',
     buttonText: 'Explore Launch Agent 3',
     bgGradient: 'from-sky-400/20 to-blue-300/20',
-    backgroundImage: '/launch-card-background.png',
+    backgroundImage: 'network-backgrounds/launch-card-background.png',
   },
 ]
 
@@ -105,7 +105,7 @@ export default function NetworksPage() {
   return (
     <main>
       <div className="container mx-auto mt-6 mb-8 px-4">
-        <h1 className="text-foreground/50 md:text-lead mb-4 text-2xl font-bold tracking-tight">
+        <h1 className="text-foreground/50 md:text- mb-4 text-3xl font-bold tracking-tight">
           Networks
         </h1>
 
