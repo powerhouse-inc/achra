b77d62bd7f60daa3134ef60e4c27afcefb605030
Yasiel Cabrera <yasiel9506@gmail.com>
2025-09-10 14:07:27 -0400

    feat(networks): integrate networks page with API

--- modules/networks/components/network-card/network-card.stories.tsx
+++ modules/networks/components/network-card/network-card.stories.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { PowerhouseLogoIsotype, PowerhouseLogotype } from '@/modules/shared/components/svgs'
+import { mockedNetworks } from '../../mocks/networks'
 import { NetworkCard } from '.'
 import type { Meta, StoryObj } from '@storybook/nextjs'
 
@@ -12,20 +11,8 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Basic: Story = {
+export const Default: Story = {
   args: {
-    isotype: (
-      <div className="flex items-center gap-2">
-        <PowerhouseLogoIsotype className="h-6 w-6" />
-        <PowerhouseLogotype className="w-40" />
-      </div>
-    ),
-    href: '/network/powerhouse',
-    tag: 'OSS',
-    variant: 'oss',
-    description:
-      'Powerhouse is the central network within the Sky ecosystem, dedicated to gathering and simplifying data for all users. It ensures that information is not only accessible but also easy to understand.',
-    buttonText: 'Explore Powerhouse',
-    backgroundImage: '/network-backgrounds/powerhouse-card-background.png',
+    profile: mockedNetworks[0],
   },
 }
--- modules/networks/utils.ts
+++
@@ -1,6 +0,0 @@
-import { type ChipVariant } from './types'
-
-export const chipStyles: Record<ChipVariant, string> = {
-  oss: 'bg-purple/30 text-purple rounded-md border-purple/70',
-  defi: 'bg-status-progress/30 text-status-progress border-status-progress/70',
-}
