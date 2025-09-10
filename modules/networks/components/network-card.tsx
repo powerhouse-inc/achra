c4b40c01e1f2513001ba491b869a370a40795815
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-10 10:38:14 -0300

    fix(achra): force to get correct padding in the botton

--- modules/networks/components/network-card.tsx
+++ modules/networks/components/network-card.tsx
@@ -39,7 +39,7 @@ export function NetworkCard({
   }
 
   const buttonStyles =
-    'text-primary-foreground bg-primary hover:bg-primary/90 inline-flex items-center justify-center rounded-md px-4 py-2 cursor-pointer'
+    'text-primary-foreground bg-primary hover:bg-primary/90 inline-flex items-center justify-center rounded-md !px-4 py-2 cursor-pointer'
 
   return (
     <Link href={href} className="h-full">
