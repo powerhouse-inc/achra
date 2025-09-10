b15807aebc8b618cd299921c2218b0f82ef382bc
Yasiel Cabrera <yasiel9506@gmail.com>
2025-09-10 10:43:19 -0400

    refactor(networks): add barrel export for network-grid component

--- app/networks/page.tsx
+++ app/networks/page.tsx
@@ -1,6 +1,6 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import { useNetworkProfileQuery } from '@/modules/__generated__/graphql/switchboard-generated'
-import { NetworkGrid } from '@/modules/networks/components/network-grid/network-grid'
+import { NetworkGrid } from '@/modules/networks/components/network-grid'
 import { NETWORK_PROFILE_DOCUMENT_ID } from '@/modules/networks/lib/constants'
 import { fetchPowerhouseNetworkProfile } from '@/modules/networks/lib/fetch-networks'
 
