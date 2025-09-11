eaaaba1cc73ca8dd0c793591a0012d21c7a5ce64
nesdc88 <nestor.diaz@dspot.com.pl>
2025-09-11 23:23:10 +0200

    fix(page): minor improvements to page and section title styles

--- modules/roadmap/components/section-title/section-title.tsx
+++ modules/roadmap/components/section-title/section-title.tsx
@@ -9,13 +9,13 @@ interface SectionTitleProps {
 export default function SectionTitle({ title, tooltip }: SectionTitleProps) {
   return (
     <div className="flex items-center gap-2">
-      <h2 className="m-0 text-lg font-bold text-gray-900 md:text-xl md:leading-6 xl:text-xl">
+      <h2 className="text-secondary-foreground m-0 text-lg font-bold md:text-xl md:leading-6 xl:text-xl">
         {title}
       </h2>
       {tooltip && (
         <Tooltip>
           <TooltipTrigger>
-            <Info className="size-4" />
+            <Info className="text-foreground/30 size-4" />
           </TooltipTrigger>
           <TooltipContent className="max-w-66" side="bottom" align="start" arrowPadding={16}>
             {tooltip}
