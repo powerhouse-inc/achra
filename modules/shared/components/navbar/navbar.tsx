fbcacb5f27e65738b03b3cca690bea998375a645
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-11 12:32:49 -0300

    fix(achra):add same behavior fussion add blur only main page

--- modules/shared/components/navbar/navbar.tsx
+++ modules/shared/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@
 import { usePathname } from 'next/navigation'
 import { useMemo } from 'react'
 
+import { cn } from '../../lib/utils'
 import { NavbarBrand } from './components/navbar-brand'
 import NavbarItemMobile from './components/navbar-item-mobile'
 import NavbarItemsDesk from './components/navbar-items-desk'
@@ -22,11 +23,12 @@ function Navbar() {
 
   const isNetworksPage = pathname === '/networks'
   const activeItem = navItems.find((item) => pathname === item.href)
+  const navbarBackgroundClass = isNetworksPage ? 'md:backdrop-blur-[40px]' : 'bg-background'
 
   return (
-    <div className="fixed top-0 right-0 left-0 z-160 flex h-24 w-full justify-center pb-2 md:backdrop-blur-[7.5px]">
-      <div className="bg-muted/30 fixed top-0 right-0 left-0 z-150 rounded-3xl p-0 shadow-lg md:mx-6 md:p-2.5 md:shadow-none xl:mx-auto xl:max-w-[1200px] 2xl:mx-14 2xl:max-w-none">
-        <header className="bg-popover flex flex-1 items-center justify-between rounded-none pr-4 md:rounded-2xl">
+    <div className={cn('fixed top-0 right-0 left-0 z-160 h-24 w-full pb-2', navbarBackgroundClass)}>
+      <div className="bg-muted/30 fixed top-0 right-0 left-0 z-150 rounded-3xl p-0 shadow-lg md:mx-6 md:p-2.5 md:shadow-none xl:container xl:px-2.5 2xl:mx-14 2xl:max-w-[calc(100%-108px)]">
+        <header className="bg-popover flex h-full flex-1 items-center justify-between rounded-none pr-4 md:rounded-2xl">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-4">
               <NavbarBrand
