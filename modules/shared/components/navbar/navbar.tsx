7148d4a0cd14b740362f6f639f96bd8e269272f4
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-11 08:57:52 -0300

    fix(achra): remove the incorrect bg-color

--- modules/shared/components/navbar/navbar.tsx
+++ modules/shared/components/navbar/navbar.tsx
@@ -24,7 +24,7 @@ function Navbar() {
   const activeItem = navItems.find((item) => pathname === item.href)
 
   return (
-    <div className="bg-red-white fixed top-0 right-0 left-0 z-160 flex h-24 w-full justify-center pb-2 md:backdrop-blur-[7.5px]">
+    <div className="fixed top-0 right-0 left-0 z-160 flex h-24 w-full justify-center pb-2 md:backdrop-blur-[7.5px]">
       <div className="bg-muted/30 fixed top-0 right-0 left-0 z-150 rounded-3xl p-0 shadow-lg md:mx-6 md:p-2.5 md:shadow-none xl:mx-auto xl:max-w-[1200px] 2xl:mx-14 2xl:max-w-none">
         <header className="bg-popover flex flex-1 items-center justify-between rounded-none pr-4 md:rounded-2xl">
           <div className="flex w-full items-center justify-between">
