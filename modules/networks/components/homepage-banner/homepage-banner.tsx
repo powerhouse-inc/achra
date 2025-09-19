ee5369b8217cabe0ff44039d92241d18f3c7b123
Ramon Guillermo <rgah2107@gmail.com>
2025-09-19 15:33:09 +0300

    fix: remove static values where possible

--- modules/networks/components/homepage-banner/homepage-banner.tsx
+++ modules/networks/components/homepage-banner/homepage-banner.tsx
@@ -36,8 +36,8 @@ export function HomepageBanner({
       open={isExpanded}
       onOpenChange={handleIsExpanded}
       className={cn(
-        'text-primary-foreground relative flex w-full flex-col gap-4 overflow-hidden rounded-xl px-6 pt-[22px] pb-[124px] transition-all duration-300 ease-in-out sm:pb-[86px] md:px-6 md:pt-8 md:pb-[23px] lg:px-8 lg:pt-8 lg:pb-[47px] xl:p-8',
-        !isExpanded && 'min-h-[154px] !py-7 pl-6 sm:min-h-[86px] lg:pl-8 xl:!py-6',
+        'text-primary-foreground relative flex w-full flex-col gap-4 overflow-hidden rounded-xl px-6 pt-8 sm:pt-5.5 pb-31 transition-all duration-300 ease-in-out sm:pb-21.5 md:px-6 md:pt-8 md:pb-5.75 lg:px-8 lg:pt-8 lg:pb-11.75 xl:p-8',
+        !isExpanded && 'min-h-38.5 !py-7 pl-6 sm:min-h-21.5 lg:pl-8 xl:!py-6',
         className,
       )}
     >
@@ -60,14 +60,14 @@ export function HomepageBanner({
       </span>
       <CollapsibleContent
         ref={collapsibleElement}
-        className="data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down z-1 max-w-[524px] overflow-hidden text-sm leading-[22px] md:max-w-[399px] lg:max-w-[647px] xl:max-w-[675px] xl:text-base xl:leading-[1.5]"
+        className="data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down z-1 max-w-131 overflow-hidden text-sm leading-5.5 md:max-w-99.75 lg:max-w-161.75 xl:max-w-168.75 xl:text-base xl:leading-[1.5]"
       >
         {description}
       </CollapsibleContent>
       {isLoggedIn && (
         <div
           className={cn(
-            'absolute right-6 bottom-4 z-1 w-[calc(100%-48px)] max-w-[295px] transition-all duration-300 ease-in-out sm:right-4 sm:w-fit md:right-6 md:bottom-6 lg:right-6 lg:bottom-6',
+            'absolute right-6 bottom-4 z-1 w-[calc(100%-48px)] max-w-73.75 transition-all duration-300 ease-in-out sm:right-4 sm:w-fit md:right-6 md:bottom-6 lg:right-6 lg:bottom-6',
             !isExpanded && '!right-[unset] !bottom-4 !left-6 sm:!right-15 sm:!left-[unset]',
           )}
         >
