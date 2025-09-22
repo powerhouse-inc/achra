d9ed2b02e346a0878b95acdb921a18738865a3e7
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-22 16:57:50 -0300

    feat(finances): add the bar chart and some utility functions

---
+++ modules/networks/components/card-bar-chart/finaces-bar-chart.tsx
@@ -0,0 +1,245 @@
+'use client'
+import ReactECharts, { type EChartsOption } from 'echarts-for-react'
+import { useMemo } from 'react'
+import { useIsMobile } from '@/modules/shared/hooks/use-mobile'
+import useFinancesBarChart from '../../hooks/use-finances-bar-chart'
+import { createTooltipFormatter, replaceAllNumberLetOneBeforeDot } from '../../sections/utils'
+import type { BarChartSeries, RevenueAndSpendingRecords } from '../../sections/types'
+
+interface FinancesBarChartProps {
+  revenueAndSpendingData: RevenueAndSpendingRecords
+  years: string[]
+}
+
+function FinancesBarChart({ revenueAndSpendingData }: FinancesBarChartProps) {
+  const { financesBarChartRef } = useFinancesBarChart()
+  const isMobile = useIsMobile()
+
+  const { chartSeries } = useMemo(() => {
+    const series: Record<string, number[]> = {
+      psm: [],
+      liquidationIncome: [],
+      fees: [],
+      dsr: [],
+      mkrVesting: [],
+      daiSpent: [],
+    }
+
+    const years = Object.keys(revenueAndSpendingData)
+      // limit the years to 2021-2024 as there's no UI space for more years
+      .filter((year) => Number(year) >= 2021 && Number(year) <= 2024)
+      .sort((a, b) => Number(a) - Number(b))
+
+    years.forEach((year) => {
+      const record = revenueAndSpendingData[year]
+      series.psm.push(record.psm)
+      series.liquidationIncome.push(record.liquidationIncome)
+      series.fees.push(record.fees)
+      series.dsr.push(record.dsr)
+      series.mkrVesting.push(record.mkrVesting)
+      series.daiSpent.push(record.daiSpent)
+    })
+
+    return {
+      chartSeries: series,
+    }
+  }, [revenueAndSpendingData])
+
+  const barWidth = 40
+
+  const series = [
+    {
+      data: chartSeries.psm,
+      type: 'bar',
+      stack: 'revenue',
+      name: 'psm',
+      barWidth,
+      itemStyle: {
+        color: '#4FC86F',
+        borderRadius: 0,
+      },
+      emphasis: {
+        itemStyle: {
+          color: 'inherit',
+        },
+      },
+    },
+    {
+      data: chartSeries.liquidationIncome,
+      type: 'bar',
+      stack: 'revenue',
+      name: 'liquidationIncome',
+      barWidth,
+      itemStyle: {
+        color: '#7AD693',
+        borderRadius: 0,
+      },
+      emphasis: {
+        itemStyle: {
+          color: 'inherit',
+        },
+      },
+    },
+    {
+      data: chartSeries.fees,
+      type: 'bar',
+      stack: 'revenue',
+      name: 'fees',
+      barWidth,
+      itemStyle: {
+        color: '#A6E3B6',
+        borderRadius: [8, 8, 0, 0],
+      },
+      emphasis: {
+        itemStyle: {
+          color: 'inherit',
+        },
+      },
+    },
+    {
+      data: chartSeries.dsr,
+      type: 'bar',
+      stack: 'spending',
+      name: 'dsr',
+      barWidth,
+      itemStyle: {
+        color: '#FFA132',
+        borderRadius: 0,
+      },
+      emphasis: {
+        itemStyle: {
+          color: 'inherit',
+        },
+      },
+    },
+    {
+      data: chartSeries.mkrVesting,
+      type: 'bar',
+      stack: 'spending',
+      name: 'mkrVesting',
+      barWidth,
+      itemStyle: {
+        color: '#F07B72',
+        borderRadius: 0,
+      },
+      emphasis: {
+        itemStyle: {
+          color: 'inherit',
+        },
+      },
+    },
+    {
+      data: chartSeries.daiSpent,
+      type: 'bar',
+      stack: 'spending',
+      name: 'daiSpent',
+      barWidth,
+      itemStyle: {
+        color: '#F4A19A',
+        borderRadius: [8, 8, 0, 0],
+      },
+      emphasis: {
+        itemStyle: {
+          color: 'inherit',
+        },
+      },
+    },
+  ]
+  const options: EChartsOption = {
+    tooltip: {
+      show: true,
+      trigger: 'axis',
+      borderRadius: 12,
+      backgroundColor: '#F2F4F7',
+      axisPointer: {
+        type: 'shadow',
+        shadowStyle: {
+          color: '#D4D9E1',
+          opacity: 0.15,
+        },
+      },
+      padding: 0,
+      borderColor: '#F2F4F7',
+      formatter: (value: BarChartSeries) => {
+        const toolTipStyle = createTooltipFormatter(isMobile)(value)
+        return toolTipStyle
+      },
+    },
+    grid: {
+      top: 8,
+      right: 0,
+      bottom: 20,
+      left: isMobile ? 40 : 49,
+    },
+    xAxis: {
+      data: ['2021', '2022', '2023', '2024'],
+      type: 'category',
+      axisLine: {
+        lineStyle: {
+          color: '#D4D9E1',
+        },
+      },
+      axisTick: {
+        show: false,
+      },
+      axisLabel: {
+        margin: 4,
+        fontFamily: 'OpenSansCondensed, san-serif',
+        fontWeight: 700,
+        fontSize: isMobile ? 12 : 14,
+        lineHeight: isMobile ? 16 : 19,
+        color: '#B6BCC2',
+      },
+      axisPointer: {
+        show: !isMobile,
+        type: 'shadow',
+        label: {
+          show: false,
+        },
+      },
+    },
+    yAxis: {
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          color: '#B6BCC2',
+        },
+      },
+      axisLine: {
+        show: true,
+        lineStyle: {
+          color: '#B6BCC2',
+        },
+      },
+      axisLabel: {
+        fontFamily: 'OpenSansCondensed, san-serif',
+        fontWeight: 700,
+        fontSize: isMobile ? 12 : 14,
+        color: '#B6BCC2',
+        formatter(value: number, index: number) {
+          if (value === 0 && index === 0) {
+            return value.toString()
+          }
+          return replaceAllNumberLetOneBeforeDot(value, true)
+        },
+      },
+    },
+    series,
+  }
+
+  return (
+    <div className="relative mt-2 flex h-[216px] w-full flex-col justify-center md:h-[253px] md:w-[385px] lg:w-[526px] xl:h-[360px] xl:min-w-[449px]">
+      <ReactECharts
+        ref={financesBarChartRef}
+        option={options}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        opts={{ renderer: 'svg' }}
+      />
+    </div>
+  )
+}
+
+export default FinancesBarChart
