dfe7aa8e576c4d2cdffcb45192fe057a948a5911
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-09-23 16:04:17 -0300

    fix(finances): add style for the legend in the bar chart

--- modules/networks/components/card-bar-chart/cards/revenue-card.tsx
+++ modules/networks/components/card-bar-chart/cards/revenue-card.tsx
@@ -1,19 +1,38 @@
 import React from 'react'
+import { cn } from '@/modules/shared/lib/utils'
 import { ItemLegend } from './legend-item'
 
 export function RevenueCard() {
   return (
     <div className="flex w-full flex-col gap-2">
-      <div className="relative flex flex-col items-center rounded-xl border border-gray-200 p-2 sm:items-start sm:border-none sm:bg-slate-50 sm:py-4 sm:pr-4 sm:pl-8 xl:p-6">
-        <span className="text-xs leading-6 font-medium text-gray-500 sm:absolute sm:-top-2.5 sm:left-10 sm:rounded-lg sm:bg-white sm:px-2 sm:py-0 md:text-sm md:leading-5 md:font-semibold xl:-top-3.5 xl:left-4 xl:text-base xl:leading-6">
+      <div
+        className={cn(
+          'sm:bg-accent relative flex flex-col items-center rounded-xl px-4 py-2 sm:items-start',
+          // border
+          'border border-gray-200 sm:rounded-xl sm:border-none',
+          // padding
+          'h-full sm:p-4 md:py-4 md:pr-4 md:pl-8 xl:h-full xl:px-6 xl:py-4',
+        )}
+      >
+        <span
+          className={cn(
+            'text-muted-foreground sm:text-popover-foreground sm:bg-popover font-medium sm:rounded-lg',
+            // position
+            'sm:absolute sm:-top-2.5 sm:left-8 xl:-top-3.5 xl:left-4',
+            // padding
+            'sm:px-2 sm:py-0',
+            // text
+            'text-xs leading-6 md:text-sm md:leading-5 md:font-semibold xl:text-base xl:leading-6',
+          )}
+        >
           Revenue
         </span>
-        <div className="flex gap-6 sm:flex-col sm:items-start sm:gap-2">
-          <ItemLegend color="fill-status-success/50 text-transparent">Fees</ItemLegend>
-          <ItemLegend color="fill-status-success/70 text-transparent">
+        <div className="flex gap-6 sm:flex-col sm:items-start sm:gap-2 xl:h-full xl:justify-center">
+          <ItemLegend color="fill-status-success/30 text-transparent">Fees</ItemLegend>
+          <ItemLegend color="fill-status-success/50 text-transparent">
             Liquidation Income
           </ItemLegend>
-          <ItemLegend color="fill-status-success text-transparent">PSM</ItemLegend>
+          <ItemLegend color="fill-status-success/70 text-transparent">PSM</ItemLegend>
         </div>
       </div>
     </div>
