8eefce82f5251bedc87f224a61d8081a87fc6d1d
Yasiel Cabrera <yasiel9506@gmail.com>
2025-09-25 16:00:17 -0400

    fix(workstream): improve responsive design for workstream card components

--- modules/shared/components/chips/workstream-status-chip/workstream-status-chip.tsx
+++ modules/shared/components/chips/workstream-status-chip/workstream-status-chip.tsx
@@ -75,7 +75,7 @@ export default function WorkstreamStatusChip({ status }: WorkstreamStatusChipPro
   return (
     <div
       className={cn(
-        'inline-flex items-center justify-center rounded-md px-3 py-px text-xs/5.5 font-semibold text-nowrap uppercase',
+        'inline-flex max-w-fit items-center justify-center rounded-md px-3 py-px text-xs/5.5 font-semibold text-nowrap uppercase',
         bgColor,
         textColor,
       )}
