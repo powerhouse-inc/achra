3f26dcb707173953ea89f3771956c6cd7a1bc159
Ramon Guillermo <rgah2107@gmail.com>
2025-10-01 18:50:42 +0300

    fix: card title section initial styles

--- modules/roadmap/components/milestone-title-section/milestone-title-section.tsx
+++ modules/roadmap/components/milestone-title-section/milestone-title-section.tsx
@@ -5,7 +5,7 @@ interface MilestoneTitleSectionProps {
 
 export default function MilestoneTitleSection({ title, description }: MilestoneTitleSectionProps) {
   return (
-    <div className="milestone-title-section bg-popover flex flex-col gap-2 rounded-xl border px-2 py-1">
+    <div className="milestone-title-section bg-popover flex flex-1 flex-col gap-2 rounded-xl border px-2 py-1">
       <h4 className="text-card-foreground line-clamp-2 text-sm leading-6 font-semibold">{title}</h4>
       {description && (
         <p className="text-card-foreground/50 line-clamp-5 text-xs leading-4.5 font-medium">
