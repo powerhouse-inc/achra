996cfbd6be2ea6a4e3a12501e5d7e0666909cd3e
Ramon Guillermo <rgah2107@gmail.com>
2025-10-02 12:27:25 +0300

    feat: implement governance external link component

---
+++ modules/networks/components/governance-section/components/external-link/external-link.tsx
@@ -0,0 +1,36 @@
+import { ExternalLinkIcon } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { cn } from '@/modules/shared/lib/utils'
+
+export interface ExternalLinkProps extends Omit<React.ComponentProps<typeof Link>, 'children'> {
+  imageSrc: string
+  name: string
+  description: string
+  className?: string
+}
+
+export function ExternalLink({ href, imageSrc, name, description, className }: ExternalLinkProps) {
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      className={cn(
+        'bg-background outline-border align-center flex w-full gap-2.5 rounded-xl p-4 shadow-sm outline lg:max-w-76 xl:max-w-96 2xl:max-w-105.5',
+        className,
+      )}
+    >
+      <div className="relative size-10">
+        <Image src={imageSrc} alt={name} fill objectFit="cover" className="absolute" />
+      </div>
+      <div className="flex flex-col">
+        <div className="text-foreground/50 flex items-center gap-1">
+          <span className="text-xs/4.5 font-medium uppercase">{description}</span>
+          <ExternalLinkIcon className="h-3 w-4" />
+        </div>
+        <span className="text-foreground text-base/6 font-semibold">{name}</span>
+      </div>
+    </Link>
+  )
+}
