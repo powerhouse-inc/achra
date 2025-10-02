713b5eaa3dce1261102f40ebb050729e1e379c54
Ramon Guillermo <rgah2107@gmail.com>
2025-10-02 13:18:38 +0300

    fix: add missing hover styles

--- modules/networks/components/governance-section/components/external-link/external-link.tsx
+++ modules/networks/components/governance-section/components/external-link/external-link.tsx
@@ -17,7 +17,7 @@ export function ExternalLink({ href, imageSrc, name, description, className }: E
       href={href}
       target="_blank"
       className={cn(
-        'bg-background outline-border align-center flex w-full gap-2.5 rounded-xl p-4 shadow-sm outline lg:max-w-76 xl:max-w-96 2xl:max-w-105.5',
+        'bg-background hover:bg-accent outline-border align-center flex w-full gap-2.5 rounded-xl p-4 shadow-sm outline lg:max-w-76 xl:max-w-96 2xl:max-w-105.5',
         className,
       )}
     >
@@ -25,7 +25,7 @@ export function ExternalLink({ href, imageSrc, name, description, className }: E
         <Image src={imageSrc} alt={name} fill className="absolute" sizes="100%" />
       </div>
       <div className="flex flex-col">
-        <div className="text-foreground/50 flex items-center gap-1">
+        <div className="text-foreground/50 hover:text-foreground flex items-center gap-1">
           <span className="text-xs/4.5 font-medium uppercase">{description}</span>
           <ExternalLinkIcon className="h-3 w-4" />
         </div>
