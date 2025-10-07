797475cec784c4936392c6b8a653042e583bbe25
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-10-07 14:48:00 -0300

    fix(breadcrumb): fix the dots colors

--- modules/shared/components/breadcrumb/components/trigger-icon.tsx
+++ modules/shared/components/breadcrumb/components/trigger-icon.tsx
@@ -3,7 +3,7 @@ import { EllipsisIcon } from 'lucide-react'
 export function TriggerIcon({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={`sm:bg-accent flex cursor-pointer items-center justify-center rounded-lg bg-transparent px-1 ${className}`}
+      className={`sm:bg-accent text-accent-foreground flex cursor-pointer items-center justify-center rounded-lg bg-transparent px-1 ${className}`}
       {...props}
     >
       <EllipsisIcon />
