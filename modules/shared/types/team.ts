b5668cbe2de9452e76513bd446fcd51d7eb6d11f
Ramon Guillermo <rgah2107@gmail.com>
2025-10-14 10:40:52 +0300

    feat: migrate all the necessary types from fusion to achra

---
+++ modules/shared/types/team.ts
@@ -0,0 +1,41 @@
+import type { ChangeTrackingEvent } from './activity'
+import type { BudgetStatement } from './budgetStatement'
+import type { ContributorCommitment, CuGithubContribution } from './contributor'
+import type { Scope } from './scopes'
+import type { SocialMediaChannels } from './socialMedia'
+import type { ResourceType, TeamStatus } from './types'
+import type { Auditor } from './users'
+
+export interface TeamUpdate {
+  id: string
+  cuId: string
+  updateTitle: string
+  updateDate: string
+  updateUrl: string
+}
+
+export interface Team {
+  id: string
+  code: string
+  shortCode: string
+  name: string
+  status: TeamStatus
+  image: string
+  budgetPath: string
+  category: string[]
+  sentenceDescription: string
+  paragraphDescription: string
+  paragraphImage: string
+  lastActivity: ChangeTrackingEvent
+  legacyBudgetStatementUrl: string
+  budgetId: string
+  type: ResourceType
+  auditors: Auditor[]
+  socialMediaChannels: SocialMediaChannels[]
+  contributorCommitment: ContributorCommitment[]
+  cuGithubContribution: CuGithubContribution[]
+  updates: TeamUpdate[]
+  scopes: Scope[]
+  budgetStatements: BudgetStatement[]
+  cuMip: null
+}
