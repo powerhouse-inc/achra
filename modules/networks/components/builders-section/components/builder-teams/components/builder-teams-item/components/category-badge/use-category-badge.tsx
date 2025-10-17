1ef97bb43e629826c27c356ac8c815aa92eda0d1
Ramon Guillermo <rgah2107@gmail.com>
2025-10-17 12:18:05 +0300

    fix: memorize values ​​to improve performance

--- modules/networks/components/builders-section/components/builder-teams/components/builder-teams-item/components/category-badge/use-category-badge.tsx
+++ modules/networks/components/builders-section/components/builder-teams/components/builder-teams-item/components/category-badge/use-category-badge.tsx
@@ -1,21 +1,25 @@
+import { useMemo } from 'react'
 import { TeamCategory } from '@/modules/shared/types/types'
 import type { ClassValue } from 'clsx'
 
 export type CategoryBadgeStyles = Record<TeamCategory, ClassValue>
 
-const categoryBadgeStyles: CategoryBadgeStyles = {
-  [TeamCategory.Technical]: 'border-status-progress/30',
-  [TeamCategory.Support]: 'border-to-do/30',
-  [TeamCategory.Operational]: 'border-primary/30',
-  [TeamCategory.Business]: 'border-status-success/30',
-  [TeamCategory.RWAs]: 'border-purple/30',
-  [TeamCategory.Growth]: 'border-destructive/30',
-  [TeamCategory.Finance]: 'border-status-progress/30',
-  [TeamCategory.Legal]: 'border-status-success/30',
-  [TeamCategory.ScopeFacilitator]: 'border-yellow/30',
-  [TeamCategory.All]: 'border-muted',
-}
-
 export const useCategoryBadge = () => {
+  const categoryBadgeStyles = useMemo<CategoryBadgeStyles>(
+    () => ({
+      [TeamCategory.Technical]: 'border-status-progress/30',
+      [TeamCategory.Support]: 'border-to-do/30',
+      [TeamCategory.Operational]: 'border-primary/30',
+      [TeamCategory.Business]: 'border-status-success/30',
+      [TeamCategory.RWAs]: 'border-purple/30',
+      [TeamCategory.Growth]: 'border-destructive/30',
+      [TeamCategory.Finance]: 'border-status-progress/30',
+      [TeamCategory.Legal]: 'border-status-success/30',
+      [TeamCategory.ScopeFacilitator]: 'border-yellow/30',
+      [TeamCategory.All]: 'border-muted',
+    }),
+    [],
+  )
+
   return { categoryBadgeStyles }
 }
--- modules/networks/components/builders-section/components/builder-teams/components/builder-teams-item/components/scope-badge/use-scope-badge.tsx
+++ modules/networks/components/builders-section/components/builder-teams/components/builder-teams-item/components/scope-badge/use-scope-badge.tsx
@@ -12,17 +12,21 @@ export interface UseScopeBadgeProps {
   scope: Scope
 }
 
-const scopeBadgeStyles: ScopeBadgeStyles = {
-  [TeamScopeEnum.SupportScope]: 'border-status-progress bg-status-progress/30 text-status-progress',
-  [TeamScopeEnum.StabilityScope]: 'border-destructive bg-destructive/30 text-destructive',
-  [TeamScopeEnum.AccessibilityScope]:
-    'border-status-success bg-status-success/30 text-status-success',
-  [TeamScopeEnum.ProtocolScope]: 'border-to-do bg-to-do/30 text-to-do',
-  [TeamScopeEnum.GovernanceScope]: 'border-primary bg-primary/30 text-primary',
-  [TeamScopeEnum.All]: 'border-muted-foreground bg-muted text-muted-foreground',
-}
-
 export const useScopeBadge = ({ size, scope }: UseScopeBadgeProps) => {
+  const scopeBadgeStyles = useMemo<ScopeBadgeStyles>(
+    () => ({
+      [TeamScopeEnum.SupportScope]:
+        'border-status-progress bg-status-progress/30 text-status-progress',
+      [TeamScopeEnum.StabilityScope]: 'border-destructive bg-destructive/30 text-destructive',
+      [TeamScopeEnum.AccessibilityScope]:
+        'border-status-success bg-status-success/30 text-status-success',
+      [TeamScopeEnum.ProtocolScope]: 'border-to-do bg-to-do/30 text-to-do',
+      [TeamScopeEnum.GovernanceScope]: 'border-primary bg-primary/30 text-primary',
+      [TeamScopeEnum.All]: 'border-muted-foreground bg-muted text-muted-foreground',
+    }),
+    [],
+  )
+
   const renderValue = useMemo(() => {
     return size === 'small' || size === 'medium' ? scope.code : scope.name
   }, [size, scope])
