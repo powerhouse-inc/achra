02c4025c5f030ac70ba0a84fb30a3fd161a7c97d
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-10-31 12:45:38 -0300

    fix(rfp): fix the size icon in 1024 resolutions

--- modules/shared/components/proposal-key-value-element/proposal-key-value-element.tsx
+++ modules/shared/components/proposal-key-value-element/proposal-key-value-element.tsx
@@ -18,7 +18,7 @@ export function ProposalKeyValueElement({
     <div className={cn('flex w-full overflow-hidden rounded-lg border', className)}>
       <div className="bg-muted text-foreground/50 flex gap-1 px-2 py-2 text-xs/4.5 font-medium sm:gap-2 sm:px-3 sm:text-sm/5.5 sm:font-semibold xl:text-base/6">
         <div className="whitespace-nowrap">{keyValue}</div>
-        <KeyIcon className="size-4 sm:size-4.5 lg:size-6" />
+        <KeyIcon className="size-4 sm:size-4.5 xl:size-6" />
       </div>
       <div className="flex items-center justify-start truncate px-2 text-sm/5.5 font-semibold sm:px-3 xl:text-base/6">
         {value}
