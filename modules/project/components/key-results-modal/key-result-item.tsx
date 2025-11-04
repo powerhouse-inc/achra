521e3f3accfdf2062c59b8b8c8cdf02b65b8fcd2
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-11-04 13:00:28 -0300

    fix(project): simplify key result item component

--- modules/project/components/key-results-modal/key-result-item.tsx
+++ modules/project/components/key-results-modal/key-result-item.tsx
@@ -16,7 +16,7 @@ export function KeyResultItem({ keyResult }: KeyResultItemProps) {
 
   return (
     <li className="border-muted mb-0! flex flex-wrap items-center justify-between gap-x-2 border-b px-2 pb-2 last:border-b-0 sm:flex-nowrap sm:gap-2">
-      <div className="flex items-center gap-1">
+      <Link href={keyResult.link as Route} target="_blank" className="flex items-center gap-1">
         <span
           className={cn(
             'h-1.5 w-1.5 shrink-0 rounded-full',
@@ -33,19 +33,8 @@ export function KeyResultItem({ keyResult }: KeyResultItemProps) {
             {keyResult.title}
           </span>
         </div>
-        {isKeyResultTodo ? (
-          <Link
-            href={`https://${keyResult.link}` as Route}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group text-muted-foreground hover:text-foreground flex items-center gap-1.5 text-sm"
-          >
-            <ExternalLink className="h-3.5 w-3.5" />
-          </Link>
-        ) : (
-          <KeyResultStatusChip />
-        )}
-      </div>
+        {isKeyResultTodo ? <ExternalLink className="h-3.5 w-3.5" /> : <KeyResultStatusChip />}
+      </Link>
       {isKeyResultTodo && (
         <div className="flex items-center gap-2 sm:w-30 md:w-48 lg:w-64">
           <span className="text-muted-foreground w-full truncate text-end text-sm">
