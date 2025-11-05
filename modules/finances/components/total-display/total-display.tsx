09cddb883c95a304cf5c81819b633ff9655729ca
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-11-05 17:22:35 -0300

    fix(project): remove unnecesary data-slot

--- modules/finances/components/total-display/total-display.tsx
+++ modules/finances/components/total-display/total-display.tsx
@@ -8,42 +8,21 @@ export function TotalDisplay({ primaryValue, secondaryValue }: TotalDisplayProps
   const secondaryValueFormatted = secondaryValue.toLocaleString() || 0
 
   return (
-    <div data-slot="total-display-container" className="flex items-center gap-3">
-      <span data-slot="total-display-title" className="text-base/6 font-semibold">
-        Total
-      </span>
-      <div
-        data-slot="total-display-content"
-        className="bg-muted flex items-center rounded-lg px-4 py-2"
-      >
-        <div data-slot="total-display-item" className="flex items-center gap-2">
-          <span
-            data-slot="total-display-value"
-            className="text-accent-foreground text-lg/6 font-semibold"
-          >
+    <div className="flex items-center gap-3">
+      <span className="text-base/6 font-semibold">Total</span>
+      <div className="bg-muted flex items-center rounded-lg px-4 py-2">
+        <div className="flex items-center gap-2">
+          <span className="text-accent-foreground text-lg/6 font-semibold">
             {primaryValueFormatted}
           </span>
-          <span
-            data-slot="total-display-description"
-            className="text-muted-foreground text-lg/6 font-semibold"
-          >
-            USDS
-          </span>
+          <span className="text-muted-foreground text-lg/6 font-semibold">USDS</span>
         </div>
-        <div data-slot="total-display-separator" className="bg-border mx-3 h-6 w-px" />
-        <div data-slot="total-display-item" className="flex items-center gap-2">
-          <span
-            data-slot="total-display-value"
-            className="text-accent-foreground text-lg/6 font-semibold"
-          >
+        <div className="bg-border mx-3 h-6 w-px" />
+        <div className="flex items-center gap-2">
+          <span className="text-accent-foreground text-lg/6 font-semibold">
             {secondaryValueFormatted}
           </span>
-          <span
-            data-slot="total-display-description"
-            className="text-muted-foreground text-lg/6 font-semibold"
-          >
-            SKY
-          </span>
+          <span className="text-muted-foreground text-lg/6 font-semibold">SKY</span>
         </div>
       </div>
     </div>
