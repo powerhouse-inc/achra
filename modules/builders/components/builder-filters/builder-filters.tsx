83fbd7e0fd7e95ec9c0830ce6aeef09bd0b32cb1
Ramon Guillermo <rgah2107@gmail.com>
2025-11-21 13:56:01 +0300

    feat: implement reset filters feature

--- modules/builders/components/builder-filters/builder-filters.tsx
+++ modules/builders/components/builder-filters/builder-filters.tsx
@@ -2,6 +2,7 @@
 
 import { FilterDrawer } from '@/modules/shared/components/filter-drawer/filter-drawer'
 import SearchInput from '@/modules/shared/components/form/search-input'
+import { Button } from '@/modules/shared/components/ui/button'
 import { Separator } from '@/modules/shared/components/ui/separator'
 import { ActorRolesSelect, ActorRolesSelectDrawer } from './actor-roles-select'
 import { ScopesSelect, ScopesSelectDrawer } from './scopes-select'
@@ -12,23 +13,22 @@ export default function BuilderFilters() {
     useBuilderFilters()
 
   return (
-    <div className="grid grid-cols-[1fr_auto] grid-rows-1 gap-4 sm:grid-cols-1 sm:grid-rows-2 md:flex lg:gap-6">
+    <div className="grid grid-cols-[1fr_auto] gap-4 sm:grid-cols-1 lg:grid-cols-[1fr_508px] lg:gap-6">
       <SearchInput
         value={search}
         onChange={(value) => void setSearch(value)}
         placeholder="Search..."
       />
-      <div className="hidden items-center gap-4 sm:flex lg:gap-6">
-        <ScopesSelect
-          scopes={scopes}
-          setScopes={setScopes}
-          className="w-1/2 md:w-full md:max-w-54 md:min-w-54 lg:max-w-64 lg:min-w-64 xl:max-w-75 xl:min-w-75"
-        />
-        <ActorRolesSelect
-          actorRoles={actorRoles}
-          setActorRoles={setActorRoles}
-          className="w-1/2 md:w-full md:max-w-54 md:min-w-54 lg:max-w-64 lg:min-w-64 xl:max-w-75 xl:min-w-75"
-        />
+      <div className="hidden items-center gap-4 sm:flex">
+        <ScopesSelect scopes={scopes} setScopes={setScopes} className="w-46" />
+        <ActorRolesSelect actorRoles={actorRoles} setActorRoles={setActorRoles} className="w-46" />
+        <Button
+          className="text-foreground/50 px-4 hover:bg-transparent dark:hover:bg-transparent"
+          variant="ghost"
+          onClick={onReset}
+        >
+          Reset Filter
+        </Button>
       </div>
 
       <div className="flex items-center gap-4 sm:hidden">
--- modules/shared/components/form/multiselect/multiselect.tsx
+++ modules/shared/components/form/multiselect/multiselect.tsx
@@ -393,7 +393,7 @@ function MultipleSelector({
         <div
           className={cn(
             'border-input bg-popover absolute top-1 z-10 w-full overflow-hidden rounded-md border shadow-xl',
-            'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
+            'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 min-w-fit',
             !open && 'hidden',
           )}
           data-state={open ? 'open' : 'closed'}
