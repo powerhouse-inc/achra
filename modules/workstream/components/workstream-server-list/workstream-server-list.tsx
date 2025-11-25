57a9d0d5fc93df19203631b278f89f41a67b8717
Yasiel Cabrera <yasiel9506@gmail.com>
2025-11-24 19:12:01 -0500

    refactor(workstream): extract workstream fetching to service layer

--- modules/workstream/components/workstream-server-list/workstream-server-list.tsx
+++ modules/workstream/components/workstream-server-list/workstream-server-list.tsx
@@ -1,9 +1,9 @@
 import { createLoader, parseAsArrayOf, parseAsString, parseAsStringEnum } from 'nuqs/server'
 import {
   type FullQueryWorkstream,
-  useWorkstreamsQuery,
   WorkstreamStatus,
 } from '@/modules/__generated__/graphql/switchboard-generated'
+import { getWorkstreams } from '../../services/workstream-service'
 import { WorkstreamCard } from '../workstream-card'
 import { WorkstreamEmpty } from '../workstream-empty'
 
@@ -30,26 +30,24 @@ async function WorkstreamServerList({ params, searchParams }: WorkstreamServerLi
   const networkFilter = slug ? [slug] : networks
   const searchFilter = search.trim().length > 0 ? search : undefined
 
-  const data = await useWorkstreamsQuery.fetcher({
-    filter: {
-      networkNames: networkFilter,
-      workstreamStatuses: statuses,
-      workstreamTitle: searchFilter,
-    },
-  })()
+  const workstreams = await getWorkstreams({
+    networkSlugs: networkFilter,
+    statuses,
+    search: searchFilter,
+  })
 
   const hasActiveFilters =
     searchFilter !== undefined ||
     statuses.length > 0 ||
     (networkFilter.length > 0 && slug === undefined)
 
-  if (data.workstreams.length === 0) {
+  if (workstreams.length === 0) {
     return <WorkstreamEmpty hasActiveFilters={hasActiveFilters} />
   }
 
   return (
     <div className="flex flex-col gap-8">
-      {data.workstreams.map((workstream) => (
+      {workstreams.map((workstream) => (
         <WorkstreamCard key={workstream.slug} workstream={workstream as FullQueryWorkstream} />
       ))}
     </div>
--- modules/workstream/services/workstream-service.ts
+++ modules/workstream/services/workstream-service.ts
@@ -1,4 +1,8 @@
-import { useWorkstreamDetailsQuery } from '@/modules/__generated__/graphql/switchboard-generated'
+import {
+  useWorkstreamDetailsQuery,
+  useWorkstreamsQuery,
+  type WorkstreamStatus,
+} from '@/modules/__generated__/graphql/switchboard-generated'
 
 /**
  * Gets the details of a workstream
@@ -21,4 +25,28 @@ export async function getWorkstreamDetails(networkSlug: string, workstreamSlug: 
   }
 
   return data.workstream[0]
+}
+
+interface GetWorkstreamsOptions {
+  search?: string
+  networkSlugs?: string[]
+  statuses?: WorkstreamStatus[]
+}
+
+/**
+ * Gets the workstreams
+ *
+ * @param options The options for the workstreams query
+ * @returns The workstreams
+ */
+export async function getWorkstreams(options: GetWorkstreamsOptions = {}) {
+  const data = await useWorkstreamsQuery.fetcher({
+    filter: {
+      networkNames: options.networkSlugs,
+      workstreamStatuses: options.statuses,
+      workstreamTitle: options.search,
+    },
+  })()
+
+  return data.workstreams
 }
