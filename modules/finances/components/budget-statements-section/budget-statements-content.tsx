80b7329e341cf614e243bbbc21733b7dd4719a10
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-12-19 10:35:46 -0300

    feat(budget):feat add load more functionality

--- modules/finances/components/budget-statements-section/budget-statements-content.tsx
+++ modules/finances/components/budget-statements-section/budget-statements-content.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/modules/shared/components/ui/button'
+import { useInfiniteArray } from '@/modules/shared/hooks/use-infinite-array'
 import { BudgetStatementsItem } from './budget-stament-item/budget-staments-item'
 import { mockBudgetStatements } from './mock/budget-stament-mock'
 import type { MetricWithoutBudget } from './type'
@@ -10,12 +11,19 @@ interface BudgetStatementsContentProps {
 export default function BudgetStatementsContent({
   budgetMetric,
 }: Readonly<BudgetStatementsContentProps>) {
+  const { visibleItems, hasMore, loadMore } = useInfiniteArray(mockBudgetStatements, {
+    firstPageSize: 10,
+    pageSize: 10,
+    enabled: true,
+  })
   return (
     <div className="flex h-fit flex-col gap-6">
-      <BudgetStatementsItem builders={mockBudgetStatements} budgetMetric={budgetMetric} />
-      <Button variant="outline" size="lg" className="w-58 self-center md:mt-0">
-        Load More
-      </Button>
+      <BudgetStatementsItem builders={visibleItems} budgetMetric={budgetMetric} />
+      {hasMore && (
+        <Button variant="outline" size="lg" className="w-58 self-center md:mt-0" onClick={loadMore}>
+          Load More
+        </Button>
+      )}
     </div>
   )
 }
