0f978e2abc29c8b9e8af04d37723e963256c4fa9
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-12-19 11:26:44 -0300

    fix(budget): add empty state for budget state

--- modules/finances/components/budget-statements-section/budget-statements-content.tsx
+++ modules/finances/components/budget-statements-section/budget-statements-content.tsx
@@ -1,4 +1,6 @@
 import { Button } from '@/modules/shared/components/ui/button'
+import { useInfiniteArray } from '@/modules/shared/hooks/use-infinite-array'
+import BudgetEmptyState from './budget-empty-state'
 import { BudgetStatementsItem } from './budget-stament-item/budget-staments-item'
 import { mockBudgetStatements } from './mock/budget-stament-mock'
 import type { MetricWithoutBudget } from './type'
@@ -10,12 +12,24 @@ interface BudgetStatementsContentProps {
 export default function BudgetStatementsContent({
   budgetMetric,
 }: Readonly<BudgetStatementsContentProps>) {
+  const { visibleItems, hasMore, loadMore } = useInfiniteArray(mockBudgetStatements, {
+    firstPageSize: 10,
+    pageSize: 10,
+    enabled: true,
+  })
+
+  const showEmptyState = visibleItems.length === 0
   return (
     <div className="flex h-fit flex-col gap-6">
-      <BudgetStatementsItem builders={mockBudgetStatements} budgetMetric={budgetMetric} />
-      <Button variant="outline" size="lg" className="w-58 self-center md:mt-0">
-        Load More
-      </Button>
+      {!showEmptyState && (
+        <BudgetStatementsItem builders={visibleItems} budgetMetric={budgetMetric} />
+      )}
+      {showEmptyState && <BudgetEmptyState />}
+      {!showEmptyState && hasMore && (
+        <Button variant="outline" size="lg" className="w-58 self-center md:mt-0" onClick={loadMore}>
+          Load More
+        </Button>
+      )}
     </div>
   )
 }
