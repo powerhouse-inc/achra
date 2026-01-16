045167435c1de2841a9b5a83cf547c5d26b04333
Yasman Mesa <yasman@dspot.com.pl>
2026-01-16 17:36:13 +0300

    test: add footer tests for dspot and powerhouse with link validations

---
+++ e2e/footer/dspot-side.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(`${process.env.HOMEPAGE_REMOTE_URL}/networks`);
+
+    await page.evaluate(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+});
+
+test('has footer links', async ({ page }) => {
+    await expect(page.getByText('X (Twitter)')).toBeVisible();
+    await expect(page.getByText('Discord')).toBeVisible();
+    await expect(page.getByText('Powered by')).toBeVisible();
+});
+
+test('has a link for Twitter', async ({ page }) => {
+    const newTabPromise = page.waitForEvent("popup");
+
+    await page.getByText('X (Twitter)').click();
+
+    const newTab = await newTabPromise;
+    await newTab.waitForLoadState();
+
+    await expect(newTab).toHaveURL('https://x.com/PowerhouseDAO');
+});
+
+test('has a link for Discord', async ({ page }) => {
+    await page.getByText('Discord').getAttribute('href').then(href => {
+        expect(href).toBe('https://discord.com/invite/pwQJwgaQKd');
+    });
+});
+
+test('has a link for Powerhouse', async ({ page }) => {
+    const newTabPromise = page.waitForEvent("popup");
+
+    await page.getByText('Powered by').click();
+
+    const newTab = await newTabPromise;
+    await newTab.waitForLoadState();
+
+    await expect(newTab).toHaveURL('https://www.powerhouse.inc/');
+});
