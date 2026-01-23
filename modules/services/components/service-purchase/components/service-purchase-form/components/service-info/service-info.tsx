6ae566e02a38b4ef7631132322057c4e52e3369c
Ramon Guillermo <rgah2107@gmail.com>
2026-01-23 10:23:29 +0300

    feat: add cta to book a call in the first step

--- modules/services/components/service-purchase/components/service-purchase-form/components/service-info/service-info.tsx
+++ modules/services/components/service-purchase/components/service-purchase-form/components/service-info/service-info.tsx
@@ -1,6 +1,9 @@
+import { Phone } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { SERVICES_CARDS_MOCK } from '@/modules/services/mocks/services'
 import { ServicesEntitiesChip } from '@/modules/shared/components/chips/services-entities-chip'
+import { Button } from '@/modules/shared/components/ui/button'
 import { Card, CardContent } from '@/modules/shared/components/ui/card'
 import { cn } from '@/modules/shared/lib/utils'
 
@@ -13,20 +16,32 @@ export default function ServiceInfo({ light }: ServiceInfoProps) {
   return (
     <Card className="border-none bg-transparent p-0 shadow-none">
       <CardContent className={cn('flex gap-6 px-0', light && 'items-center gap-2')}>
-        <div
-          className={cn(
-            'relative size-64 min-w-64',
-            light &&
-              'border-background size-14 min-w-14 overflow-hidden rounded-full border-2 shadow-sm',
-          )}
-        >
-          <Image
-            src={service.cover}
-            alt={service.title}
-            fill
-            className="absolute rounded-lg"
-            style={{ objectFit: 'cover' }}
-          />
+        <div className={cn('flex flex-col gap-4', light && 'gap-0')}>
+          <div
+            className={cn(
+              'relative size-64 min-w-64',
+              light &&
+                'border-background size-14 min-w-14 overflow-hidden rounded-full border-2 shadow-sm',
+            )}
+          >
+            <Image
+              src={service.cover}
+              alt={service.title}
+              fill
+              className="absolute rounded-lg"
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <Button variant="outline" className={cn('w-full', light && 'hidden')} asChild>
+            <Link
+              href="mailto:powerhousetest@gmail.com?subject=Book%20a%20call&body=I%20would%20like%20to%20book%20a%20call"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Book a Call
+              <Phone className="size-4" />
+            </Link>
+          </Button>
         </div>
         <div className={cn('flex flex-col gap-2', light && 'gap-0')}>
           <span className={cn('text-foreground text-xl/6 font-bold', light && 'text-2xl/7')}>
