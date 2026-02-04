c70768ac3a4958acdc4b80d635467e6c72a9f3c2
Ramon Guillermo <rgah2107@gmail.com>
2026-02-04 15:00:12 +0300

    fix: builder profile page content loading state

--- modules/builder-profile/components/builder-profile/builder-profile-skeleton.tsx
+++ modules/builder-profile/components/builder-profile/builder-profile-skeleton.tsx
@@ -4,7 +4,15 @@ import { cn } from '@/modules/shared/lib/utils'
 function AboutUsSkeleton() {
   return (
     <div className="flex w-full flex-col gap-4">
-      <Skeleton className="h-6 w-62" />
+      <div className="flex items-center justify-between">
+        <Skeleton className="h-6 w-62" />
+        <div className="flex items-center gap-2 lg:hidden">
+          <Skeleton className="size-4 sm:size-13.5" />
+          <Skeleton className="size-4 sm:hidden" />
+          <ConnectButtonSkeleton className="hidden sm:flex" />
+        </div>
+      </div>
+      <ConnectButtonSkeleton className="w-full sm:hidden" />
       <div className="flex flex-col gap-2">
         <Skeleton className="mb-1.5 h-5.5 w-[10%]" />
         <Skeleton className="h-3.75 w-[91%]" />
@@ -44,32 +52,30 @@ interface ActionsSkeletonProps {
 
 function ConnectButtonSkeleton({ className }: ActionsSkeletonProps) {
   return (
-    <div className={cn('bg-accent flex w-56.25 flex-col gap-6 rounded-md', className)}>
-      <div className="flex justify-end">
-        <div className="flex items-center gap-4 py-1.5 pr-2 pl-4">
-          <Skeleton className="bg-border size-7.5 rounded-full" />
-          <div className="flex flex-col gap-1">
-            <div className="flex items-center gap-1">
-              <Skeleton className="bg-border h-4.5 w-24.25" />
-              <Skeleton className="bg-border size-3 rounded-sm" />
-            </div>
-            <Skeleton className="bg-border h-4 w-38.25" />
+    <div className={cn('bg-accent flex w-56.25 max-w-85 gap-6 rounded-md', className)}>
+      <div className="flex items-center gap-4 py-2 pr-2 pl-4">
+        <Skeleton className="bg-border size-7.5 rounded-full" />
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-1">
+            <Skeleton className="bg-border h-4.5 w-24.25" />
+            <Skeleton className="bg-border size-3 rounded-sm" />
           </div>
+          <Skeleton className="bg-border h-4 w-38.25" />
         </div>
       </div>
     </div>
   )
 }
 
-function FinancesSkeleton() {
+function FinancesSkeleton({ className }: ActionsSkeletonProps) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className={cn('flex flex-col gap-2', className)}>
       <Skeleton className="h-6 w-22.25" />
       <div className="bg-popover flex w-full flex-col gap-4 rounded-xl p-4 shadow-sm">
         <Skeleton className="h-5.5 w-full" />
-        <div className="flex gap-4">
-          <Skeleton className="h-9 w-46.5" />
-          <Skeleton className="h-9 w-29.25" />
+        <div className="grid w-full grid-cols-[55%_1fr] gap-4">
+          <Skeleton className="h-9 w-full" />
+          <Skeleton className="h-9 w-full" />
         </div>
         <div className="border-border bg-background relative mt-2 flex w-full gap-6 rounded-lg border p-4">
           <div className="border-popover absolute -top-2.5 left-4 h-4.5 border-x-8">
@@ -89,6 +95,29 @@ function FinancesSkeleton() {
   )
 }
 
+function SkillsSkeleton() {
+  return (
+    <div className="bg-popover flex w-full flex-col gap-2 rounded-xl px-4 pt-2 pb-4 shadow-sm">
+      <div className="flex justify-between gap-4">
+        <Skeleton className="h-6 w-10" />
+        <Skeleton className="size-4" />
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Skeleton className="h-5.5 w-44" />
+        <Skeleton className="h-5.5 w-38" />
+        <Skeleton className="h-5.5 w-52" />
+        <Skeleton className="h-5.5 w-47" />
+        <Skeleton className="h-5.5 w-38" />
+        <Skeleton className="h-5.5 w-40" />
+        <Skeleton className="h-5.5 w-18" />
+        <Skeleton className="h-5.5 w-20" />
+        <Skeleton className="h-5.5 w-14" />
+        <Skeleton className="h-5.5 w-22" />
+      </div>
+    </div>
+  )
+}
+
 function ProjectSkeleton() {
   return (
     <div className="border-border bg-popover flex w-full flex-col gap-4 rounded-xl border p-3 shadow-sm">
@@ -103,8 +132,8 @@ function ProjectSkeleton() {
         <Skeleton className="h-4 w-[90%]" />
         <Skeleton className="h-4 w-[44%]" />
       </div>
-      <div className="flex flex-col gap-4 xl:flex-row xl:justify-between">
-        <div className="border-border bg-popover flex w-full flex-col gap-2 rounded-lg border px-4 py-2 sm:w-104.5">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="border-border bg-popover flex w-full flex-col gap-2 rounded-lg border px-4 py-2">
           <Skeleton className="h-4.5 w-10.5" />
           <div className="flex items-center justify-between">
             <div className="flex gap-1">
@@ -114,7 +143,7 @@ function ProjectSkeleton() {
             <Skeleton className="h-4.5 w-10" />
           </div>
         </div>
-        <div className="border-border bg-popover flex w-full flex-col gap-2 rounded-lg border px-4 py-2 sm:w-104.5">
+        <div className="border-border bg-popover flex w-full flex-col gap-2 rounded-lg border px-4 py-2">
           <Skeleton className="h-4.5 w-10.5" />
           <div className="flex items-center justify-between">
             <div className="flex gap-1">
@@ -124,7 +153,7 @@ function ProjectSkeleton() {
             <Skeleton className="h-4.5 w-10" />
           </div>
         </div>
-        <div className="border-border bg-popover flex w-full flex-col gap-2 rounded-lg border px-4 py-2 sm:w-104.5">
+        <div className="border-border bg-popover flex w-full flex-col gap-2 rounded-lg border px-4 py-2">
           <Skeleton className="h-4.5 w-10.5" />
           <div className="flex items-center justify-between">
             <div className="flex gap-1">
@@ -142,13 +171,11 @@ function ProjectSkeleton() {
 function BuilderProfileSkeleton() {
   return (
     <div className="flex flex-col gap-8">
-      <div className="grid grid-cols-1 gap-8 lg:grid-cols-[55%_1fr] xl:grid-cols-[65%_1fr]">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-[65%_1fr] xl:grid-cols-[60%_1fr] 2xl:grid-cols-[65%_1fr]">
         <AboutUsSkeleton />
         <div className="flex flex-col gap-6">
-          <div className="flex justify-end">
-            <ConnectButtonSkeleton />
-          </div>
-          <FinancesSkeleton />
+          <FinancesSkeleton className="hidden lg:flex" />
+          <SkillsSkeleton />
         </div>
       </div>
       <div className="flex flex-col gap-6">
