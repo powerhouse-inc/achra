0d6373f39ea9079dcb9ee808b4e148aec03866eb
Ramon Guillermo <rgah2107@gmail.com>
2026-02-10 13:27:12 +0300

    feat: implement dashboard page error and loading state

--- app/network/[slug]/(network-dashboard)/loading.tsx
+++ app/network/[slug]/(network-dashboard)/loading.tsx
@@ -1,9 +1,14 @@
+import { BuildersSectionSkeleton } from '@/modules/dashboard/components/builders-section/builders-section-skeleton'
+import { FinancesSectionSkeleton } from '@/modules/dashboard/components/finances-section/finances-section-skeleton'
+import { HomepageBannerSkeleton } from '@/modules/networks/components/homepage-banner/homepage-banner-skeleton'
 import { PageContent } from '@/modules/shared/components/page-containers'
 
 export default function NetworkPageLoading() {
   return (
     <PageContent className="gap-8">
-      <div>Loading...</div>
+      <HomepageBannerSkeleton />
+      <BuildersSectionSkeleton />
+      <FinancesSectionSkeleton />
     </PageContent>
   )
 }
