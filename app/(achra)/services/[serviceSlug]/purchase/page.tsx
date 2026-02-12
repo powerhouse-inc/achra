0928866e0dddc38bda5f387a4fef3c883cfc09b6
Ramon Guillermo <rgah2107@gmail.com>
2026-02-12 18:20:54 +0300

    fix: service purchase page param issue

--- app/(achra)/services/[serviceSlug]/purchase/page.tsx
+++ app/(achra)/services/[serviceSlug]/purchase/page.tsx
@@ -4,18 +4,18 @@ import { ErrorBoundaryWithPresets } from '@/modules/shared/components/error-stat
 import { PageContent } from '@/modules/shared/components/page-containers'
 
 interface ServicePurchasePageProps {
-  params: Promise<{ docId: string }>
+  params: Promise<{ serviceSlug: string }>
 }
 
 export default async function ServicePurchasePage({ params }: ServicePurchasePageProps) {
-  const { docId } = await params
+  const { serviceSlug } = await params
 
   return (
     <PageContent className="gap-6">
       <ErrorBoundaryWithPresets>
         {/* TODO: Implement the missing skeletons components for the service purchase page */}
         <Suspense fallback={<div>Loading...</div>}>
-          <ServicePurchase docId={docId} />
+          <ServicePurchase docId={serviceSlug} />
         </Suspense>
       </ErrorBoundaryWithPresets>
     </PageContent>
