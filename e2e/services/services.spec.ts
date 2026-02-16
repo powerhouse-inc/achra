7d040061ca79101d53538e0abf8ff483f9f48b9e
Yasman Mesa <yasman@dspot.com.pl>
2026-02-16 10:44:44 +0300

    test: update tab visibility and refine service counts in services tests for improved accuracy

--- e2e/services/services.spec.ts
+++ e2e/services/services.spec.ts
@@ -5,12 +5,10 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('should contain all tabs', async ({ page }) => {
-    await expect(page.getByRole('tab', { name: 'SNO Governors' })).toBeVisible();
-    await expect(page.getByRole('tab', { name: 'Founders' })).toBeVisible();
-    await expect(page.getByRole('tab', { name: 'Operators' })).toBeVisible();
+    await expect(page.getByRole('tab', { name: 'Networks' })).toBeVisible();
     await expect(page.getByRole('tab', { name: 'Builders' })).toBeVisible();
     await expect(page.getByRole('tab', { name: 'All' })).toBeVisible();
-    await expect(page.getByRole('tab')).toHaveCount(5);
+    await expect(page.getByRole('tab')).toHaveCount(3);
 });
 
 test('should contain search input', async ({ page }) => {
@@ -18,19 +16,27 @@ test('should contain search input', async ({ page }) => {
 });
 
 test('should contain services or resource links to resource items', async ({ page }) => {
-    await expect(page.getByText('Purchase')).toHaveCount(6);
-    await expect(page.getByText('More info')).toHaveCount(6);
-    await expect(page.getByText('SNO Embryonic Hub')).toHaveCount(1);
-    await expect(page.getByText('SNO Governors')).toHaveCount(6);
-    await expect(page.getByText('Founders')).toHaveCount(2);
-    await expect(page.getByText('Operators')).toHaveCount(3);
-    await expect(page.getByText('Builders')).toHaveCount(3);
-    await expect(page.getByText('Formation & Setup')).toHaveCount(6);
-    await expect(page.getByText('Recurring Services')).toHaveCount(6);
+    await expect(page.getByText('Purchase')).toHaveCount(3);
+    await expect(page.getByText('More info')).toHaveCount(3);
+    await expect(page.getByText('SNO Embryonic Hub')).toHaveCount(0);
+    await expect(page.getByText('SNO Governors')).toHaveCount(0);
+    await expect(page.getByText('Founders')).toHaveCount(0);
+    await expect(page.getByText('Operators')).toHaveCount(0);
+    await expect(page.getByText('Builders')).toHaveCount(2);
+    await expect(page.getByText('Formation & Setup')).toHaveCount(3);
+    await expect(page.getByText('Recurring Services')).toHaveCount(3);
 });
 
 test('should navigate to more info page by clicking on the "More Info" button', async ({ page }) => {
-    await page.getByRole('tab', { name: 'SNO Governors' }).click();
+    await page.getByRole('tab', { name: 'Builders' }).click();
     await page.getByRole('link', { name: 'More Info' }).first().click();
-    await expect(page).toHaveURL(`${process.env.HOMEPAGE_REMOTE_URL}/services/sno-embryonic-hub`);
+    await expect(page).toHaveURL(`${process.env.HOMEPAGE_REMOTE_URL}/services/c6aacdfe-b182-4ec5-8a4c-dbf9f21708f8`);
+});
+
+test('should search a service by name', async ({ page }) => {
+    await page.getByPlaceholder('Search...').fill('Sky Ecosystem');
+    await expect(page.getByText('No services found')).toBeVisible();
+
+    await page.getByPlaceholder('Search...').fill('COMMERCIAL OPERATIONAL HUB (OH)');
+    await expect(page.getByText('Purchase')).toHaveCount(1);
 });
