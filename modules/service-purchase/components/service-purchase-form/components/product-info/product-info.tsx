f8e2180dcf743fd8c1b7aa47cdf459555fbe310f
Ramon Guillermo <rgah2107@gmail.com>
2026-02-17 14:36:11 +0300

    fix: the product description should be wraped in a markdown

--- modules/service-purchase/components/service-purchase-form/components/product-info/product-info.tsx
+++ modules/service-purchase/components/service-purchase-form/components/product-info/product-info.tsx
@@ -5,6 +5,7 @@ import type {
   ResourceTemplate_ContentSection,
 } from '@/modules/__generated__/graphql/switchboard-generated'
 import ExpandableSection from '@/modules/service-purchase/components/service-purchase-form/components/product-info/components/expandable-section'
+import { Markdown } from '@/modules/shared/components/markdown/markdown'
 
 export interface ProductInfoProps {
   description?: Maybe<string>
@@ -14,7 +15,7 @@ export interface ProductInfoProps {
 export default function ProductInfo({ description, contentSections }: ProductInfoProps) {
   return (
     <div className="flex flex-col gap-6">
-      <span className="text-foreground text-sm/5.5 lg:text-base/6">{description}</span>
+      <Markdown className="text-foreground">{description ?? ''}</Markdown>
       <div className="flex flex-col gap-4">
         {contentSections.map((section: ResourceTemplate_ContentSection) => (
           <ExpandableSection key={section.id} title={section.title} description={section.content} />
