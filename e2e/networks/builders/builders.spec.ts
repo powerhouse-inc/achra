6be60742813a148b359dbaced240a0a741882aee
Yasman Mesa <yasman@dspot.com.pl>
2026-02-20 11:22:31 +0300

    test: update builder status and skill counts, refine last modified value assertions for improved accuracy in builder tests

--- e2e/networks/builders/builders.spec.ts
+++ e2e/networks/builders/builders.spec.ts
@@ -34,25 +34,24 @@ test('should load all builder statuses', async ({ page }) => {
     await expect(page.getByText('COMPLETED')).toHaveCount(0);
     await expect(page.getByText('ON HOLD')).toHaveCount(0);
     await expect(page.getByText('ARCHIVED')).toHaveCount(0);
-    await expect(page.getByText('ACTIVE')).toHaveCount(8);
+    await expect(page.getByText('ACTIVE')).toHaveCount(14);
     await expect(page.getByText('INACTIVE')).toHaveCount(0);
 });
 
 test('should load all builder skills', async ({ page }) => {
-    await expect(page.getByText('Facilitator')).toHaveCount(9);
-    await expect(page.getByText('Backend Development')).toHaveCount(3);
-    await expect(page.getByText('Full Stack Development')).toHaveCount(6);
+    await expect(page.getByText('Facilitator')).toHaveCount(15);
+    await expect(page.getByText('Backend Development')).toHaveCount(6);
+    await expect(page.getByText('Full Stack Development')).toHaveCount(9);
     await expect(page.getByText('Devops Engineering')).toHaveCount(0);
     await expect(page.getByText('Smart Contract Development')).toHaveCount(0);
-    await expect(page.getByText('UI/UX Design')).toHaveCount(0);
-    await expect(page.getByText('Technical Writing')).toHaveCount(0);
+    await expect(page.getByText('UI/UX Design')).toHaveCount(3);
+    await expect(page.getByText('Technical Writing')).toHaveCount(3);
     await expect(page.getByText('QA Testing')).toHaveCount(0);
     await expect(page.getByText('Data Engineering')).toHaveCount(0);
     await expect(page.getByText('Security Engineering')).toHaveCount(0);
-    await expect(page.getByText('Technical Writing')).toHaveCount(0);
 
     await page.locator('table > tbody > tr:nth-child(1) > td > a > div > div.hidden > div:nth-child(2) > div').click();
-    await expect(page.getByText('UI/UX Design')).toHaveCount(1);
+    await expect(page.getByText('UI/UX Design')).toHaveCount(4);
 });
 
 test.skip('should load all builder scopes', async ({ page }) => {
@@ -67,10 +66,9 @@ test.skip('should load all builder scopes', async ({ page }) => {
 });
 
 test('should load all last modified values', async ({ page }) => {
-    await expect(page.getByText('9 Days Ago')).toHaveCount(2);
-    await expect(page.getByText('3 Days Ago')).toHaveCount(4);
-    await expect(page.getByText('06-FEB-2026')).toHaveCount(1);
-    await expect(page.getByText('12-FEB-2026')).toHaveCount(2);
+    await expect(page.getByText('16-FEB-2026')).toHaveCount(1);
+    await expect(page.getByText('12-FEB-2026')).toHaveCount(1);
+    await expect(page.getByText('18-FEB-2026')).toHaveCount(4);
 });
 
 test('should load all builder links', async ({ page }) => {
