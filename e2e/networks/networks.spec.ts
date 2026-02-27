a36fd9fabc0582a8e580ef6f1903bba1f0b7f3f7
Yasman Mesa <yasman@dspot.com.pl>
2026-02-27 14:35:04 +0300

    test: refactor locator to use text-based selection for improved clarity in network page navigation test

--- e2e/networks/networks.spec.ts
+++ e2e/networks/networks.spec.ts
@@ -3,8 +3,7 @@ import { test, expect } from '@playwright/test';
 test('should navigate to the network page by clicking on the network card', async ({ page }) => {
     await page.goto(`${process.env.HOMEPAGE_REMOTE_URL}/networks`);
 
-    //TODO: refactor locator
-    await page.locator('a[href="/network/powerhouse"]').first().click();
+    await page.getByText('Explore Powerhouse').click();
 
     await expect(page).toHaveURL(`${process.env.HOMEPAGE_REMOTE_URL}/network/powerhouse`);
 });
