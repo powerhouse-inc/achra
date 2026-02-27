935b9e2d71557bbb761440305a6050b09dcb0134
Yasman Mesa <yasman@dspot.com.pl>
2026-02-27 12:00:56 +0300

    test: refactor locator usage in homepage tests for improved clarity and maintainability

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -5,22 +5,19 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('has a link to the Networks page', async ({ page }) => {
-    //TODO: refactor locator
-    await page.locator('a.framer-11lmli7.framer-122cz0u').getByText('Networks').click();
+    await page.getByRole('link', { name: 'Networks', exact: true }).click();
 
     await expect(page).toHaveURL(`${process.env.HOMEPAGE_REMOTE_URL}/networks`);
 });
 
 test('has a link to the Services page', async ({ page }) => {
-    //TODO: refactor locator
-    await page.locator('a.framer-18lak58.framer-122cz0u').getByText('Services').click();
+    await page.getByRole('link', { name: 'Services', exact: true }).click();
 
     await expect(page).toHaveURL(`${process.env.HOMEPAGE_REMOTE_URL}/services`);
 });
 
 test('has a link to the Use Cases page', async ({ page }) => {
-    //TODO: refactor locator
-    await page.locator('a.framer-1cgu14x.framer-122cz0u').getByText('Use Cases').click();
+    await page.getByRole('navigation').getByRole('link', { name: 'Use Cases', exact: true }).click();
 
     await expect(page).toHaveURL(`${process.env.HOMEPAGE_REMOTE_URL}/cases`);
 });
@@ -33,21 +30,10 @@ test('has a link to the Networks page in the "For Organizations" section', async
     await expect(page).toHaveURL('https://achra.com/networks');
 });
 
-test('has a link to the Workstreams page in the "For Builders" section', async ({ page }) => {
+test('has a description in the "For Builders" section', async ({ page }) => {
     await page.waitForLoadState('networkidle');
     await page.getByText('For Builders').click();
     await expect(page.getByText('Discover active projects and roadmaps from leading network organizations. Focus on building with built-in operational support.')).toBeVisible();
-
-    /* This block was hidden for the MVP
-    await expect(page.getByText('Builder Opportunities')).toBeVisible();
-
-    const newTabPromise = page.waitForEvent("popup");
-    
-    await page.getByText('Builder Opportunities').click()
-    const newTab = await newTabPromise;
-    await newTab.waitForLoadState();
-
-    await expect(newTab).toHaveURL('https://achra.com/workstreams'); */
 });
 
 test('has a link to the Services page in the "For Operators" section', async ({ page }) => {
@@ -89,7 +75,7 @@ test('has a link to the Powerhouse SNO page from the link "Visit Powerhouse"', a
     await expect(newTab).toHaveURL('https://powerhouse.io/');
 });
 
-// This needs a VPN, if it is run in Belarus.
+// This needs a VPN, if it runs in a restricted region for the Cloudflare waitlist.
 test('has a button to subscribe to the Powerhouse waitlist', async ({ page }) => {
     await page.waitForLoadState('networkidle');
     await page.getByPlaceholder('Drop your email for launch updates.').fill('test@test.com');
