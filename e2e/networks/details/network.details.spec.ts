9698767aa309b79c424a6a9b3034abdc4249e8a6
Yasman Mesa <yasman@dspot.com.pl>
2026-02-27 14:56:03 +0300

    test: refactor network details spec to use dynamic network variables and improve locator clarity

--- e2e/networks/details/network.details.spec.ts
+++ e2e/networks/details/network.details.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
+const network = 'powerhouse';
+const networkName = 'Powerhouse';
+
 test.beforeEach(async ({ page }) => {
-    await page.goto(`${process.env.HOMEPAGE_REMOTE_URL}/network/powerhouse`);
+    await page.goto(`${process.env.HOMEPAGE_REMOTE_URL}/network/${network}`);
 });
 
 test('should contract the top banner', async ({ page }) => {
-    const description = "The Powerhouse dashboard provides an overview of the builder teams and their financial data within the Powerhouse network. This portal tracks technical expertise, team information, and monthly financial reporting to maintain transparency across all active workstreams. It serves as the primary tool for monitoring team capabilities and fiscal oversight for every project operating under the Powerhouse network."
+    const description = `The ${networkName} dashboard provides an overview of the builder teams and their financial data within the ${networkName} network. This portal tracks technical expertise, team information, and monthly financial reporting to maintain transparency across all active workstreams. It serves as the primary tool for monitoring team capabilities and fiscal oversight for every project operating under the ${networkName} network.`
     await expect(page.getByText(description)).toBeVisible();
 
-    //TODO: refactor locator
-    await page.locator('div[data-slot="collapsible"] > button').click();
+    await page.locator('main.container button').first().click();
     await expect(page.getByText(description)).toBeHidden();
 
-    //TODO: refactor locator
-    await page.locator('div[data-slot="collapsible"] > button').click();
+    await page.locator('main.container button').first().click();
     await expect(page.getByText(description)).toBeVisible();
 });
