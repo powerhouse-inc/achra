836dee3abec4c2d07f8ab044009a6a73e44d0c1e
Ramon Guillermo <rgah2107@gmail.com>
2026-03-02 15:36:31 +0300

    fix: eslint issue

--- modules/finances/components/expenses-metric-chart/expenses-metric-chart-card.tsx
+++ modules/finances/components/expenses-metric-chart/expenses-metric-chart-card.tsx
@@ -7,7 +7,9 @@ interface ExpensesMetricChartCardProps {
   children: ReactNode
 }
 
-export default function ExpensesMetricChartCard({ children }: Readonly<ExpensesMetricChartCardProps>) {
+export default function ExpensesMetricChartCard({
+  children,
+}: Readonly<ExpensesMetricChartCardProps>) {
   return (
     <Card className="bg-popover flex w-full flex-col gap-4 border-none p-2 pb-4 shadow-xs md:p-4 lg:p-6">
       <Suspense fallback={<div className="h-20 animate-pulse" />}>
