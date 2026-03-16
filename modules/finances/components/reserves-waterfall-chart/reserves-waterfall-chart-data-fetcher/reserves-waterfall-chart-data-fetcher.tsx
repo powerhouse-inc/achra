3f8a6067a1c0df1b0e2eacb4dc1493e2dfc3c93f
Ramon Guillermo <rgah2107@gmail.com>
2026-03-16 14:47:00 +0300

    fix: rollback reserve component api changes

---
+++ modules/finances/components/reserves-waterfall-chart/reserves-waterfall-chart-data-fetcher/reserves-waterfall-chart-data-fetcher.tsx
@@ -0,0 +1,33 @@
+import { financesReservesChartSearchParamsCache } from '@/modules/finances/lib/finances-reserves-chart-search-params'
+import { ATLAS_BUDGETS } from '@/modules/finances/mocks/group-wallets'
+import { getBudgetsByCodePath, getCodePathFromParams } from '@/modules/finances/utils'
+import { ReservesWaterfallChart } from '../reserves-waterfall-chart'
+
+interface ReservesWaterfallChartDataFetcherProps {
+  params: Promise<{
+    slug: string
+    financeSlug?: string[]
+  }>
+  searchParams: Promise<Record<string, string | string[] | undefined>>
+}
+
+async function ReservesWaterfallChartDataFetcher({
+  params,
+  searchParams,
+}: Readonly<ReservesWaterfallChartDataFetcherProps>) {
+  const { financeSlug } = await params
+  const { year } = financesReservesChartSearchParamsCache.parse(await searchParams)
+  const codePath = getCodePathFromParams(financeSlug)
+  const budgets = getBudgetsByCodePath(codePath, ATLAS_BUDGETS)
+
+  return (
+    <ReservesWaterfallChart
+      codePath={codePath}
+      budgets={budgets}
+      allBudgets={ATLAS_BUDGETS}
+      year={year}
+    />
+  )
+}
+
+export { ReservesWaterfallChartDataFetcher }
