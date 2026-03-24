265a25739e531a173213550443586f071a658912
Ramon Guillermo <rgah2107@gmail.com>
2026-03-24 13:23:18 +0300

    feat: implement finances page loading state

--- modules/finances/components/budget-statements-section/budget-statements-section-wrapper.tsx
+++ modules/finances/components/budget-statements-section/budget-statements-section-wrapper.tsx
@@ -1,15 +1,11 @@
-import { Suspense } from 'react'
 import { BudgetStatementsContentWrapper } from '@/modules/finances/components/budget-statements-section/budget-statements-content-wrapper'
-import { BudgetStatementsSectionSkeleton } from '@/modules/finances/components/budget-statements-section/budget-statements-skeleton/budget-statements-section-skeleton'
 import { BudgetStatementsTitle } from '@/modules/finances/components/budget-statements-section/budget-statements-title'
-import { BudgetStatementsTitleSkeleton } from '@/modules/finances/components/budget-statements-section/budget-statements-title-skeleton'
 import { FinancesSections } from '@/modules/finances/components/config/const'
 import { ErrorBoundaryWithPresets } from '@/modules/shared/components/error-state/error-boundry-with-presets'
 import { encodeSectionId } from '@/modules/shared/components/section-activation'
 import { SCROLL_MT_CLASSES } from '@/modules/shared/config/constants'
 import { cn } from '@/modules/shared/lib/utils'
 import { BudgetStatementFilters } from './budget-statement-filters/budget-statement-filters'
-import { BudgetStatementFiltersSkeleton } from './budget-statement-filters/budget-statement-filters-skeleton'
 
 interface BudgetStatementsSectionWrapperProps {
   params: Promise<{
@@ -31,17 +27,11 @@ export function BudgetStatementsSectionWrapper({
       id={encodeSectionId(FinancesSections.BudgetStatements)}
     >
       <div className="flex w-full items-center justify-between gap-4">
-        <Suspense fallback={<BudgetStatementsTitleSkeleton />}>
-          <BudgetStatementsTitle />
-        </Suspense>
-        <Suspense fallback={<BudgetStatementFiltersSkeleton />}>
-          <BudgetStatementFilters />
-        </Suspense>
+        <BudgetStatementsTitle />
+        <BudgetStatementFilters />
       </div>
       <ErrorBoundaryWithPresets description="We ran into an unexpected error while loading the budget statements. Please try again later.">
-        <Suspense fallback={<BudgetStatementsSectionSkeleton />}>
-          <BudgetStatementsContentWrapper params={params} searchParams={searchParams} />
-        </Suspense>
+        <BudgetStatementsContentWrapper params={params} searchParams={searchParams} />
       </ErrorBoundaryWithPresets>
     </section>
   )
