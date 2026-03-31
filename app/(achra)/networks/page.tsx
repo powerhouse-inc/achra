b1199fc75452b2cda070c32480a584abb081eb0b
Yasiel Cabrera <yasiel9506@gmail.com>
2026-03-31 14:31:42 -0400

    Revert "feat(networks): integrate network notification signup with Mailchimp"

--- app/(achra)/networks/page.tsx
+++ app/(achra)/networks/page.tsx
@@ -1,14 +1,17 @@
 import { Suspense } from 'react'
 import { Networks } from '@/modules/networks/components/networks/networks'
 import { NetworksSkeleton } from '@/modules/networks/components/networks/networks-skeleton'
+import { ErrorBoundaryWithPresets } from '@/modules/shared/components/error-state'
 import { PageContent } from '@/modules/shared/components/page-containers'
 
 export default function NetworksPage() {
   return (
     <PageContent>
-      <Suspense fallback={<NetworksSkeleton />}>
-        <Networks />
-      </Suspense>
+      <ErrorBoundaryWithPresets>
+        <Suspense fallback={<NetworksSkeleton />}>
+          <Networks />
+        </Suspense>
+      </ErrorBoundaryWithPresets>
     </PageContent>
   )
 }
